4337a0c231f3368a9f8383f6f852a35328da0ebf
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2025-10-02 11:48:38 +0700

    optimize

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
     server: {
         host: "0.0.0.0",
         port: 5173,
+        watch: {
+            usePolling: true,
+            interval: 100,
+        },
         hmr: {
             host: "localhost",
         },
