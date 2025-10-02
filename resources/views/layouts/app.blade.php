8d1c3d8a3207af328c3d25d03d51d54887abc6d8
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2025-10-02 15:01:07 +0700

    Fix font-awesome don't show on production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Jong-Oun') }}</title>
 
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
 </head>
