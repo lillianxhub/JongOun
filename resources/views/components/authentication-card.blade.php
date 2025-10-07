579863bb11b0c380a2d72a586dc7ea2068920476
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2025-10-07 16:47:08 +0700

    redesign login/register

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,7 +1,7 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-transparent">
 
-    <a href="{{ route('home') }}" class="text-3xl font-bold">JongOun</a>
-    <div class="w-full sm:max-w-lg mt-6 px-10 py-8 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <a href="{{ route('home') }}" class="text-3xl font-bold text-white">JongOun</a>
+    <div class="w-full sm:max-w-lg mt-6 px-10 py-8 bg-dark shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
