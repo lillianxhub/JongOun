4c4bd9dacb216c9af739b0b25247d5facde9ae7c
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2025-10-12 20:41:31 +0700

    add comment for explain

--- resources/views/livewire/room-manager.blade.php
+++ resources/views/livewire/room-manager.blade.php
@@ -185,7 +185,6 @@
                                 <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
                             @enderror
                         </div>
-
                 </div>
 
                 <!-- Form Actions -->
